aa734c0db06462eb6747ffaf42e859a9c7ae2deb
Ojwang1 <wayanicholas@gmail.com>
2022-04-06 11:02:19 +0300

    initial commit

---
+++ app/src/main/java/com/example/myfirebasedatabaseapp/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.myfirebasedatabaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    var mEdtName:EditText ? =null
+    var mEdtEmail:EditText ? =null
+    var mEdtNumber:EditText ? =null
+    var mBtnSave:Button ? =null
+    var mBtnView:Button ? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mEdtName=findViewById(R.id.mEdtName)
+        mEdtEmail=findViewById(R.id.mEdtEmail)
+        mEdtNumber=findViewById(R.id.mEdtNumber)
+        mBtnSave=findViewById(R.id.mBtnSave)
+        mBtnView=findViewById(R.id.mBtnView)
+
+        mBtnSave!!.setOnClickListener {
+            var name=mEdtName!!.text.toString().trim()
+            var email=mEdtEmail!!.text.toString().trim()
+            var idNumber=mEdtNumber!!.text.toString().trim()
+            if (name.isEmpty()){
+                mEdtName!!.setError("Please fill this input")
+                mEdtName!!.requestFocus()
+            }else if (email.isEmpty()){
+                mEdtEmail!!.setError("pleae fill this input")
+                mEdtEmail!!.requestFocus()
+            }else if (idNumber.isEmpty()){
+                mEdtNumber!!.setError("Please fill this input")
+                mEdtNumber!!.requestFocus()
+            }else{
+                var time=System.currentTimeMillis().toString()
+                var userData=User(name,email,idNumber,time)
+                var ref = FirebaseDatabase.getInstance().getReference().child("User/$time")
+                ref.setValue(userData).addOnCompleteListener {
+                        task->
+                    if(task.isSuccessful){
+                        Toast.makeText(applicationContext,"User save successfully",
+                            Toast.LENGTH_LONG).show()
+                    }else{ Toast.makeText(applicationContext,"Saving user failed",
+                            Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/myfirebasedatabaseapp/User.kt
@@ -0,0 +1,19 @@
+package com.example.myfirebasedatabaseapp
+
+class User {
+    var name:String=""
+    var email:String=""
+    var Number:String=""
+    var id:String=""
+
+    constructor(name: String, email: String, Number: String, id: String) {
+        this.name = name
+        this.email = email
+        this.Number = Number
+        this.id = id
+
+    }
+
+    constructor(){}
+
+}
